b371f63f175532515f1983a9075a982221b7484c
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2021-03-15 21:47:14 -0300

    comienzo

---
+++ CarritoCompras/configuracion.php
@@ -0,0 +1,11 @@
+<?php
+
+$PROYECTO = "FinalPWD_0321/CarritoCompras";
+
+$GLOBALS['ROOT'] = $_SERVER['DOCUMENT_ROOT'] . "/FinalPWD_0321/CarritoCompras/";
+
+$INICIO = "Location:http://" . $_SERVER['HTTP_HOST'] . "$PROYECTO" . "/vista/index/home.php";
+
+include_once("utiles/funciones.php");
+
+//echo $_SERVER['DOCUMENT_ROOT'];
